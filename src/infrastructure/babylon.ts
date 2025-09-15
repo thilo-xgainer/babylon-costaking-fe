da7681d5f06f1af970aa2d668e4b98f2dc500b28
ngutudu <ngutuduhust@gmail.com>
2025-09-15 09:06:38 +0700

    feat: use query contract

--- src/infrastructure/babylon.ts
+++ src/infrastructure/babylon.ts
@@ -1,8 +1,10 @@
 import { createLCDClient, txs, utils } from "@babylonlabs-io/babylon-proto-ts";
+import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 
 import { getNetworkConfigBBN } from "@/ui/common/config/network/bbn";
 
 let clientInstance: Awaited<ReturnType<typeof createLCDClient>> | null = null;
+let contractClientInstance: CosmWasmClient | null = null;
 
 export const getBabylonClient = async () => {
   if (!clientInstance) {
@@ -12,8 +14,17 @@ export const getBabylonClient = async () => {
   return clientInstance;
 };
 
+export const getCosmWasmClient = async () => {
+  if (!contractClientInstance) {
+    const networkConfig = getNetworkConfigBBN();
+    contractClientInstance = await CosmWasmClient.connect(networkConfig.rpc);
+  }
+  return contractClientInstance;
+};
+
 export default {
   client: getBabylonClient,
+  cosmWasmClient: getCosmWasmClient,
   txs,
   utils,
 };
